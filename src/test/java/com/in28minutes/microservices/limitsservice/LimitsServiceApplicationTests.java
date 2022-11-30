39d8d8459cc1c3538c7ffa1ae44f75bdedcdec2a
lewisc99 <lewiscontato99@gmail.com>
2022-11-30 14:56:41 -0300

    mock a debug test

--- src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
+++ src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.in28minutes.microservices.limitsservice;
 
+import com.in28minutes.microservices.limitsservice.controller.LimitsController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class LimitsServiceApplicationTests {
+
+
 
 	@Test
 	void contextLoads() {
@@ -15,5 +20,14 @@ class LimitsServiceApplicationTests {
 	@Test
 	void simpleTest()
 	{}
+
+	@Test
+	void another_test()
+	{
+		LimitsController limitsController = new LimitsController();
+
+		assertNull(limitsController);
+
+	}
 
 }
